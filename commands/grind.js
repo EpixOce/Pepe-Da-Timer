546e075a005a1820712ebd7428d1d73efb813568
Epi <34924285+EpixOce@users.noreply.github.com>
2019-03-30 10:06:57 +1300

    Update and rename grind.js to uwu.js

--- commands/grind.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send("**UwU**, this bot is made by **Epi** - A guy who definitely knows how to code")
-}
-
-module.exports.help = {
-    name: "grind"
-}
---
+++ commands/uwu.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send("**UwU**, this bot is made by **Epi** btw")
+}
+
+module.exports.help = {
+    name: "uwu"
+}
